2769dd0d3319ec397c90532fe628525f164d5c1c
Martin Henry <martinhenry@unknownb8e8560b02a0.att.net>
2014-07-16 22:13:23 -0700

    updated angular app with first question request

--- gothai.js
+++ gothai.js
@@ -1,12 +1,33 @@
 angular.module('todoApp', [])
 
 .controller('GoThaiController', ['$scope', '$q', '$http', function($scope, $q, $http) {
+
+  var questionNum = null;
+  // var questionNum = 123;
+
   $scope.submitWord = function() {
+    var params = JSON.stringify({word:$scope.word});
     var deferred = $q.defer();
-    $http.post('http://localhost:4747/submitWord', $scope.word)
+    $http.post('http://localhost:4747/submitWord', params)
       .then(function(data){
         deferred.resolve(data);
       })
     return deferred.promise;
+  }
+
+  var loadQuestion = function() {
+    var params = JSON.stringify({questionNumber:questionNum});
+    var deferred = $q.defer();
+    $http.post('http://localhost:4747/getQuestion', params)
+      .then(function(data){
+        $scope.currentQuestion = data.data;
+        console.log(data)
+        deferred.resolve(data);
+      })
+    return deferred.promise;
+  }
+
+  $scope.init = function() {
+    loadQuestion();
   }
 }]);
